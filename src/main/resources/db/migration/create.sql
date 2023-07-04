fb7403ccc30539cb1bd2be765a2b132f1935c94b
kitamuraDev <masato1999824@icloud.com>
2023-07-04 23:14:09 +0900

    :tada: Setup done

---
+++ src/main/resources/db/migration/create.sql
@@ -0,0 +1,6 @@
+CREATE TABLE characters(
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(100)
+);
+
+DROP TABLE characters;
---
+++ src/test/java/spring/data/jpa/demo/springdatajpademo/SpringDataJpaDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package spring.data.jpa.demo.springdatajpademo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringDataJpaDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
