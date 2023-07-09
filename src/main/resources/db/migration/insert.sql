187c8176cf6cd8190732b4235f3a2e0ddd99f34c
kitamuraDev <masato1999824@icloud.com>
2023-07-09 14:41:40 +0900

    :bento: まとめてinsertできるようにDDLを修正

--- src/main/resources/db/migration/insert.sql
+++ src/main/resources/db/migration/insert.sql
@@ -1,7 +1,4 @@
-INSERT INTO characters (name) VALUES ('桜島 麻衣');
-INSERT INTO characters (name) VALUES ('梓川 咲太');
-INSERT INTO characters (name) VALUES ('梓川 花楓');
-INSERT INTO characters (name) VALUES ('豊浜 のどか');
-INSERT INTO characters (name) VALUES ('牧之原 翔子');
-INSERT INTO characters (name) VALUES ('古賀 朋絵');
-INSERT INTO characters (name) VALUES ('双葉 理央');
+INSERT INTO characters (name) VALUES
+('桜島 麻衣'), ('梓川 咲太'), ('梓川 花楓'),
+('豊浜 のどか'), ('牧之原 翔子'), ('古賀 朋絵'),
+('双葉 理央');
