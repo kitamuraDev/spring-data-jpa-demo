ee9c9e42799157446bbbe844762c04a5e11477eb
kitamuraDev <masato1999824@icloud.com>
2023-07-12 22:33:04 +0900

    :bento: 1対1の2テーブルに変更

--- src/main/resources/db/migration/create.sql
+++ src/main/resources/db/migration/create.sql
@@ -1,6 +1,15 @@
-CREATE TABLE character(
+CREATE TABLE IF NOT EXISTS character(
     id SERIAL PRIMARY KEY,
     name VARCHAR(100)
 );
 
-DROP TABLE character;
+CREATE TABLE IF NOT EXISTS character_info(
+    id SERIAL PRIMARY KEY,
+    character_id INT,
+    birth_date DATE,
+    gender CHAR(1),
+    Foreign Key (character_id) REFERENCES character (id)
+);
+
+-- DROP TABLE character;
+-- DROP TABLE character_info;
--- src/main/resources/db/migration/insert.sql
+++ src/main/resources/db/migration/insert.sql
@@ -2,3 +2,8 @@ INSERT INTO character (name) VALUES
 ('桜島 麻衣'), ('梓川 咲太'), ('梓川 花楓');
 -- ('豊浜 のどか'), ('牧之原 翔子'), ('古賀 朋絵'),
 -- ('双葉 理央');
+
+INSERT INTO character_info (character_id, birth_date, gender) VALUES
+(1, '1998-12-02', '女'),
+(2, '1999-04-10', '男'),
+(3, '2002-11-05', '女');
